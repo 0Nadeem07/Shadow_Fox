0ac7b69c9106f8ceccdad44e33ca67976ee20424
0Nadeem07 <nadeemansari1842@gmail.com>
2024-06-05 14:32:44 +0530

    first 2 problems completed with all sub-problems

--- task1/numbers.py
+++ task1/numbers.py
@@ -1,15 +1,9 @@
-'''
-
-3. If you cross a 490meterlong street in 7 minutes, calculate your
-speed in meters per second. Print the answer without any decimal
-point in it. Hint: Speed = Distance / Time'''
-
-
 
 '''1. Write a function that takes two arguments, 145 and 'o'
 , and uses the `format` function to return a formatted string. Print the
 result. Try to identify the representation used.'''
 
+print("Subproblem 1")
 def func(int ,char):
     return "{} students passed with grade {}.".format(int ,char)
 
@@ -19,7 +13,7 @@ print(result)
 # output => 145 students passed with grade O.
 
 
-
+print("\n")
 '''2. In a village, there is a circular pond with a radius of 84 meters.
 Calculate the area of the pond using the formula: Circle Area = π
 r^2. (Use the value 3.14 for π) Bonus Question: If there is exactly
@@ -28,11 +22,27 @@ water in the pond? Print the answer without any decimal point in
 it. Hint: Circle Area = π r^2 Water in the pond = Pond Area/
 Water per Square Meter'''
 
+print("Subproblem 2")
 def pond(r ,pi):
     area = int(pi*r*r)
-    print("Total area of the pond is ",area)
+    print(f"Total area of the pond is {area} m^2")
 
     total_water = int(area / 1.4)
     print("Total amount of water in the pond is ",total_water ," litres")
 
 pond(84,3.14)
+
+print("\n")
+
+'''3. If you cross a 490meterlong street in 7 minutes, calculate your
+speed in meters per second. Print the answer without any decimal
+point in it. Hint: Speed = Distance / Time'''
+
+print("Subproblem 3")
+distance = 490 
+t_min= 7
+t_sec = t_min *60 # converting minute into seconds
+
+speed = int(distance /(t_sec)) 
+
+print(f"Speed is {speed} m/s^2")
